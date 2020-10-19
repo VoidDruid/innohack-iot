5f7529f7ebce692bba47e9a16b7f83421ad439e3
Kovalev Vasiliy <minskij10@yandex.ru>
2020-10-19 22:57:00 +0300

    Add first draft

--- master/src/main.cpp
+++ master/src/main.cpp
@@ -1,9 +1,44 @@
-#include <Arduino.h>
-
+// библиотека для работы с HTTP-протоколом
+#include <HTTPClient.h>
+// вводим имя и пароль точки доступа
+const char* ssid = "";
+const char* password = "";
+ 
 void setup() {
-  // put your setup code here, to run once:
+    // иницилизируем монитор порта
+    Serial.begin(9600);
+    // запас времени на открытие монитора порта — 5 секунд
+    delay(5000);
+    // подключаемся к Wi-Fi сети
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Connecting to Wi-Fi..");
+    }
+    Serial.println("Connected to the Wi-Fi network");
 }
-
+ 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // выполняем проверку подключения к беспроводной сети
+    if ((WiFi.status() == WL_CONNECTED)) {
+        // создаем объект для работы с HTTP
+        HTTPClient http;
+        // подключаемся к тестовому серверу с помощью HTTP
+        http.begin("http://drop-table.tech/api");
+        // делаем GET запрос
+        int httpCode = http.GET();
+        // проверяем успешность запроса
+        if (httpCode > 0) {
+            // выводим ответ сервера
+            String payload = http.getString();
+            Serial.println(httpCode);
+            Serial.println(payload);
+        }
+        else {
+            Serial.println("Error on HTTP request");
+        }
+        // освобождаем ресурсы микроконтроллера
+        http.end();
+    }
+    delay(10000);
 }
